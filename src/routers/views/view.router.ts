2364fd0d71543c06d699a38de5b7f43532e8ee00
taeyoung0001 <her020212@naver.com>
2024-09-20 21:44:41 +0900

    profile changeNumbers 완료

--- src/routers/views/view.router.ts
+++ src/routers/views/view.router.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import { ROUTES_INDEX } from "..";
-import { extractPath } from "@/utils/path.util";
-import UsersController from "@/api/users/controller/users.controller";
-import { UsersServiceImpl } from "@/api/users/service/users.service";
-import { MongooseUserRepository } from "@/api/users/repository/user/mongooseUser.repository";
-import { MongooseProfileRepository } from "@/api/users/repository/profile/mongooseProfile.repository";
+// import { ROUTES_INDEX } from "..";
+// import { extractPath } from "@/utils/path.util";
+// import UsersController from "@/api/users/controller/users.controller";
+// import { UsersServiceImpl } from "@/api/users/service/users.service";
+// import { MongooseUserRepository } from "@/api/users/repository/user/mongooseUser.repository";
+// import { MongooseProfileRepository } from "@/api/users/repository/profile/mongooseProfile.repository";
 
 const viewRouter = express.Router();
-const usersController = new UsersController(
-  new UsersServiceImpl(
-    new MongooseUserRepository(),
-    new MongooseProfileRepository()
-  )
-);
+// const usersController = new UsersController(
+//   new UsersServiceImpl(
+//     new MongooseUserRepository(),
+//     new MongooseProfileRepository()
+//   )
+// );
 const BASE_PATH = "/views";
 
 viewRouter.get(`${BASE_PATH}/login`, (req, res) => {

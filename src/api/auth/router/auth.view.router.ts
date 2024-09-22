da1c9b814228e35382f055b8cc36ecd9a359aedb
ehddk <bj9147@naver.com>
2024-09-22 22:56:22 +0900

    라우터에 회원가입 로직 추가

--- src/api/auth/router/auth.view.router.ts
+++ src/api/auth/router/auth.view.router.ts
@@ -8,6 +8,8 @@ import { AuthServiceImpl } from "../service/auth.service";
 import { MongooseUserRepository } from "@/api/users/repository/user/mongooseUser.repository";
 import { UsersServiceImpl } from "@/api/users/service/users.service";
 import { MongooseProfileRepository } from "@/api/users/repository/profile/mongooseProfile.repository";
+import { createUserValidator } from "@/api/users/dto/validations/users.validation"
+import {validate} from "@/api/common/middlewares/validation.middleware"
 // import AuthViewController from "../controller/auth.view.controller";
 // import { AuthServiceImpl } from "../service/auth.service";
 // import { MongooseUserRepository } from "@/api/users/repository/user/mongooseUser.repository";
@@ -47,10 +49,17 @@ authViewRouter.get(
 
 authViewRouter.get(
   extractPath(AUTH_VIEW_ROUTES.SIGN_UP, ROUTES_INDEX.AUTH_VIEW),
+ // validate(createUserValidator),
   authViewController.signUpPage
   // AUTH_VIEW_ROUTES.SIGN_UP,(req,res)=>{
   //   res.render('client/auth/signup')
   // }
 );
+authViewRouter.post(
+  extractPath(AUTH_VIEW_ROUTES.SIGN_UP, ROUTES_INDEX.AUTH_VIEW),
+  validate(createUserValidator),
+  authViewController.signup // 실제 회원가입 처리 로직
+);
+
 
 export default authViewRouter;

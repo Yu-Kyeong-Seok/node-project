ddbe3cbd31a85ec6f8797e0a0cb20386d27ae388
ella227 <ella227@gmail.com>
2024-09-23 09:11:41 +0900

    [장유진] 리포지토리 인터페이스 생성

---
+++ src/api/faq/repository/faq.repository.ts
@@ -0,0 +1,12 @@
+export interface FaqRepository {
+  /** FAQ 생성 */
+  save(faq: Omit<IFaq, "id">): Promise<IFaq>;
+  /** FAQ 목록 조회 */
+  findAll(): Promise<IFaq[]>;
+  /** ID로 FAQ 조회 */
+  findById(id: string): Promise<IFaq | null>;
+    /** FAQ 수정 */
+  update(faqId: string, updateFaqInfo: Partial<IFaq>): Promise<void>;
+    /** FAQ 삭제 */
+  delete(faqId: string): Promise<void>;
+}

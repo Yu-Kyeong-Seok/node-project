6a2ba16da3f7ffcd87653df1701d2411d024df1e
ehddk <bj9147@naver.com>
2024-09-30 20:54:14 +0900

    공지사항 & faq 페이지 경로 변경

--- src/api/notice/router/notice.view.router.ts
+++ src/api/notice/router/notice.view.router.ts
@@ -7,14 +7,17 @@ import { authCookieViewMiddleware } from "@/api/common/middlewares/authCookie.mi
 const noticeViewRouter = express.Router();
 const noticesViewController = new NoticesViewController(new NoticesServiceImpl(new MongooseNoticeRepository()));
 
-const BASE_PATH = "/views";
+const NOTICE_VIEW_ROUTES={
+  GET_NOTICES:`/notice`,
+  GET_NOTICEDETAIL:`/notice/:noticeId`
+}
 
 
-noticeViewRouter.get(`${BASE_PATH}/notice`, (req, res, next) => {
+noticeViewRouter.get(NOTICE_VIEW_ROUTES.GET_NOTICES, (req, res, next) => {
   noticesViewController.getNoticesView(req, res, next); 
 });
 
-noticeViewRouter.get(`${BASE_PATH}/notice/:noticeId`, (req, res, next) => {
+noticeViewRouter.get(NOTICE_VIEW_ROUTES.GET_NOTICEDETAIL, (req, res, next) => {
   noticesViewController.getNoticeDetailView(req, res, next); 
 });
 
